d16c98bf7033eadbe4923a6d39b91d31ccfefde0
dy-wqy <752016959@qq.com>
2022-10-24 20:32:34 +0200

    week 3

---
+++ src/week3/args.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+def get_args():
+    parser =ArgumentParser()
+
+    parser.add_argument("-epochs", type=int, default=0, dest = "epochs", help="number of epochs")
+    parser.add_argument("-batch_size", type=int, default=64,dest="batch_size")
+    parser.add_argument("-lr",type=float,default=0.001,help="learning rate")
+
+    parser.add_argument("-input_dim",type=int,default=768)
+    parser.add_argument("-hidden_dim",type=int,default=128)
+    parser.add_argument("-output_dim",type=int,default=2)
+
+    args = parser.parse_args()
+
+    return args
+
