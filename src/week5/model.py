0a97727a18c32e78e993b4e80e7a09c8ce6a5c24
dy-wqy <752016959@qq.com>
2022-10-24 20:21:25 +0200

    week 5

---
+++ src/week5/model.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import DistilBertForTokenClassification, DistilBertConfig
+
+
+class attention(nn.Module):
+    def __init__(self, hidden_dim, attn_drop):
+        super(attention, self).__init__()
+        self.fc = nn.Linear(hidden_dim, hidden_dim, bias=True)
+        nn.init.xavier_normal_(self.fc.weight, gain=1.414)
+
+        self.tanh = nn.Tanh()
+        self.att = nn.Parameter(torch.empty(size=(1, hidden_dim)), requires_grad=True)
+        nn.init.xavier_normal_(self.att.data, gain=1.414)
+
+        self.softmax = nn.Softmax()
+        if attn_drop:
+            self.attn_drop = nn.Dropout(attn_drop)
+        else:
+            self.attn_drop = lambda x: x
+
+    def forward(self, embeds):
+        beta = []
+        attn_curr = self.attn_drop(self.att)
+        for embed in embeds:
+            sp = self.tanh(self.fc(embed)).mean(dim=0)
+            beta.append(attn_curr.matmul(sp.t()))
+        beta = torch.cat(beta, dim=-1).view(-1)
+        beta = self.softmax(beta)
+        # print(ntype+" mp ", beta.data.cpu().numpy())  # semantic attention
+        z_mp = 0
+        for i in range(len(embeds)):
+            z_mp = z_mp + embeds[i] * beta[i]
+        return z_mp
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(MLP, self).__init__()
+        self.layer1 = nn.Linear(input_dim, hidden_dim)
+        self.layer2 = nn.Linear(hidden_dim, 32)
+        self.layer3 = nn.Linear(32, output_dim)
+
+    def forward(self, data):
+        data = F.relu(self.layer1(data))
+        data = F.relu(self.layer2(data))
+        data = self.layer3(data)
+        return data
+
+
+def mean_pooling(model_output, attention_mask):
+    # Mean Pooling - Take attention mask into account for correct averaging
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(model_output.size()).float()
+    sum_embeddings = torch.sum(model_output * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
+
+
+class QA_model(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(QA_model, self).__init__()
+        self.que_in_mlp = nn.Linear(input_dim, hidden_dim)
+        self.context_in_mlp = nn.Linear(input_dim, hidden_dim)
+        self.output_layer = MLP(input_dim, hidden_dim, output_dim)
+        self.attention_layer = attention(hidden_dim, 0.5)
+        self.num_labels = 3
+        configuration = DistilBertConfig()
+        configuration.num_labels = self.num_labels
+        self.transformer_layer = DistilBertForTokenClassification(configuration)
+
+    def forward(self, input):
+        input_ids = torch.stack(input["input_ids"], dim=1).cuda()
+        attention_mask = torch.stack(input["attention_mask"], dim=1).cuda()
+        labels = torch.stack(input['label'], dim=1).cuda()
+        # question_input_ids = torch.stack(input['question_input_ids'],dim=0).cuda()
+        # question_attention_mask=torch.stack(input['question_attention_mask'],dim=0).cuda()
+        # document_input_ids = torch.stack(input['document_input_ids'],dim=0).cuda()
+        # document_attention_mask = torch.stack(input['document_attention_mask'],dim=0).cuda()
+        hidden_states = self.transformer_layer(input_ids=input_ids, attention_mask=attention_mask,
+                                               output_hidden_states=True, return_dict=True)
+        # hidden_out = mean_pooling(hidden_states[-1], attention_mask)
+        predict_label = hidden_states['logits']
+        predict_label = predict_label.reshape(-1, self.num_labels)
+
+        return predict_label
---
+++ src/week5/tokenizer.py
@@ -0,0 +1,155 @@
+import torchtext
+import torch
+from datasets import load_dataset
+from torchtext.data.utils import get_tokenizer
+from collections import Counter
+from torchtext.vocab import Vocab
+import gensim
+from transformers import AutoTokenizer
+from sentence_transformers import SentenceTransformer
+
+dataset = load_dataset("copenlu/answerable_tydiqa")
+train_set = dataset["train"]
+validation_set = dataset["validation"]
+model_checkpoint = "bert-base-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+
+
+def getLanguageDataSet(data, language):
+    return data.filter(lambda x: x['language'] == language)
+
+
+def getJapaneseDataSet(data):
+    return getLanguageDataSet(data, "japanese")
+
+
+def getEnglishDataSet(data):
+    return getLanguageDataSet(data, "english")
+
+
+def getFinnishDataSet(data):
+    return getLanguageDataSet(data, "finnish")
+
+
+def build_vocab(dataSet, tokenizer):
+    counter = Counter()
+    for data in dataSet:
+        counter.update(tokenizer(data['document_plaintext']))
+    return Vocab(counter)
+
+
+def getWord2VecModel(train_dataSet, test_dataSet, tokenizer):
+    sentences = []
+    keys = ["document_plaintext", "question_text"]
+    for element in train_dataSet:
+        for key in keys:
+            sentences.append(tokenizer(element[key]))
+    for element in test_dataSet:
+        for key in keys:
+            sentences.append(tokenizer(element[key]))
+    w2v_model = gensim.models.Word2Vec(sentences, vector_size=128, min_count=1, window=3, epochs=15)
+
+    w2v_model.wv.save_word2vec_format("vector.txt", binary=False)
+    return w2v_model
+
+
+def data_process_continuous(dataSet, SbertModel, tokenizer, tokenPart="document"):
+    data = []
+    for element in dataSet:
+        if tokenPart == "document":
+            en_tensor_ = torch.tensor([SbertModel.encode(element["document_plaintext"])], dtype=torch.float32).cuda()
+            data.append(en_tensor_)
+        elif tokenPart == "question":
+            en_tensor_ = torch.tensor([SbertModel.encode((element["question_text"]))], dtype=torch.float32).cuda()
+            data.append(en_tensor_)
+        elif tokenPart == "answer":
+            if (element["annotations"]["answer_start"] == [-1]):
+                # en_tensor_ = torch.tensor([w2vModel.get_vector(token) for token in tokenizer(element["annotations"]["answer_text"])], dtype=torch.float32)
+                # data.append(en_tensor_)
+                data.append(torch.tensor([0], dtype=torch.int64).cuda())
+            else:
+                data.append(torch.tensor([1], dtype=torch.int64).cuda())
+    return torch.cat(data, dim=0)
+
+
+def tokenize_function(examples):
+    return tokenizer(examples, padding="max_length", truncation=True)
+
+
+def data_process_transformer(dataSet, w2vModel, tokenizer, tokenPart="document"):
+    if tokenPart == 'answer':
+        data = []
+    else:
+        data = {"input_ids": [], 'attention_mask': []}
+    for element in dataSet:
+        if tokenPart == "document":
+            en_tensor_ = tokenize_function(element["document_plaintext"])
+
+            data["input_ids"].append(torch.tensor(en_tensor_['input_ids']).cuda())
+            data["attention_mask"].append(torch.tensor(en_tensor_['attention_mask']).cuda())
+        elif tokenPart == "question":
+            en_tensor_ = tokenize_function(element["question_text"])
+
+            data["input_ids"].append(torch.tensor(en_tensor_['input_ids']).cuda())
+            data["attention_mask"].append(torch.tensor(en_tensor_['attention_mask']).cuda())
+        elif tokenPart == "answer":
+            if (element["annotations"]["answer_start"] == [-1]):
+                data.append(torch.tensor([0], dtype=torch.int64).cuda())
+            else:
+                data.append(torch.tensor([1], dtype=torch.int64).cuda())
+    if tokenPart == 'answer':
+        data = torch.cat(data, dim=0)
+    else:
+        data["input_ids"] = torch.stack(data["input_ids"], dim=0)
+        data["attention_mask"] = torch.stack(data["attention_mask"], dim=0)
+    return data
+
+
+def data_process(dataSet, w2vModel, tokenizer, tokenPart="document"):
+    data = []
+    for element in dataSet:
+        if tokenPart == "document":
+            en_tensor_ = torch.tensor(
+                [w2vModel.get_vector(token) for token in tokenizer(element["document_plaintext"])], dtype=torch.float32)
+            en_tensor_ = torch.mean(en_tensor_, dim=0, keepdim=True).cuda()
+            data.append(en_tensor_)
+        elif tokenPart == "question":
+            en_tensor_ = torch.tensor([w2vModel.get_vector(token) for token in tokenizer(element["question_text"])],
+                                      dtype=torch.float32)
+            en_tensor_ = torch.mean(en_tensor_, dim=0, keepdim=True).cuda()
+            data.append(en_tensor_)
+        elif tokenPart == "answer":
+            if (element["annotations"]["answer_start"] == [-1]):
+                # en_tensor_ = torch.tensor([w2vModel.get_vector(token) for token in tokenizer(element["annotations"]["answer_text"])], dtype=torch.float32)
+                # data.append(en_tensor_)
+                data.append(torch.tensor([0], dtype=torch.int64).cuda())
+            else:
+                data.append(torch.tensor([1], dtype=torch.int64).cuda())
+    return torch.cat(data, dim=0)
+
+
+def getEnglishData(data, tokenPart="document"):
+    # tokenizer = get_tokenizer('basic_english', language="en")
+    englishDataSet = getEnglishDataSet(data)
+
+    # w2vModel=getWord2VecModel(train_set,tokenizer)
+    # w2vModel = gensim.models.KeyedVectors.load_word2vec_format("vector.txt",binary=False)
+    SbertModel = SentenceTransformer('all-MiniLM-L6-v2')
+    return data_process_transformer(englishDataSet, SbertModel, tokenizer, tokenPart)
+
+
+# todo Japanese的tokenizer
+def getJapaneseData(data):
+    tokenizer = get_tokenizer('basic_english', language="en")
+    dataset = getJapaneseDataSet(data)
+    vocab = build_vocab(dataset, tokenizer)
+    return data_process(dataset, vocab, tokenizer)
+
+
+# todo Finnish的tokenizer
+def getFinnishData(data):
+    tokenizer = get_tokenizer('basic_english', language="en")
+    dataset = getFinnishDataSet(data)
+    vocab = build_vocab(dataset, tokenizer)
+    return data_process(dataset, vocab, tokenizer)
+
